336bf50a659c14e0685bf27796e1a958335ac102
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2021-05-13 19:53:50 -0300

    Adiciona script para avaliar biblioteca

---
+++ evaluate_lib.py
@@ -0,0 +1,46 @@
+import subprocess
+
+fibonacci_input = range(7, 36, 7)
+virtual_processors = range(3, 16, 3)
+
+output_file = "Evaluation.csv"
+
+header = "," + "".join([f"{vp}," for vp in virtual_processors]) + '\n'
+
+with open(output_file, 'w') as output:
+    output.write(header)
+
+
+def get_time(fib_output):
+    fib, _, time_output, *_ = fib_output.split('\n')
+    _, real_time = time_output.split('\t')
+
+    print(fib)
+
+    return_time = real_time.replace('m', ':') \
+                           .replace(',', '.')
+
+    return return_time[:-1]
+
+
+for fib_in in fibonacci_input:
+    with open(output_file, 'a') as output:
+        output.write(f"{fib_in},")
+
+    for pv in virtual_processors:
+        cmd = f"time ./exemplo {pv} {fib_in}"
+
+        # Executa comando
+        output = subprocess.check_output(['bash', '-c', cmd],
+                                         stderr=subprocess.STDOUT)
+        output_str = output.decode("utf-8")
+
+        # Pega saída
+        time = get_time(output_str)
+
+        # Salva em arquivo de saída
+        with open(output_file, 'a') as output:
+            output.write(f"{time},")
+
+    with open(output_file, 'a') as output:
+        output.write("\n")
